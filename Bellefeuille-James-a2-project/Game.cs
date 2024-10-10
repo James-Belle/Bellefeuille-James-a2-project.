67b39be5e115305841262955c97aeefa8311d732
James Bellefeuille <spyro9046@outlook.com>
2024-10-10 17:49:22 -0400

    seperate hunger and heat, finish most of cat, add reactive background, improve furniture add food system and item holding system

--- Bellefeuille-James-a2-project/Game.cs
+++ Bellefeuille-James-a2-project/Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include code libraries you need below (use the namespace).
 using System;
+using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices;
 
@@ -13,17 +14,24 @@ namespace Game10003
     {
         // Place your variables here:
         bool leftLeg = true;
-        bool window = false;
         bool Fire = false;
         int pawTime = 0;
         Color brightOrange = new Color(180, 100, 50);
         Color darkOrange = new Color(150, 70, 40);
 
         Color Brown = new Color(80, 40, 10);
+        Color coldBrown = new Color(100, 70, 60);
+        Color hotBrown = new Color(130, 30, 0);
+
+        Color windowBlue = new Color(100, 110, 200);
+
         static int frameRate = 20; //how many frames till animation updates
-        int[] catStats = [0, 0];
+        int tempature = 0;
+        int hunger = 0;
         bool windowOpen = false;
         int heaterWoodLeft = 0;
+        int[] food = [-1, -1, -1, -1, -1];
+        string held = "none"; // this can be none, food, or wood.
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
@@ -39,12 +47,10 @@ namespace Game10003
         /// </summary>
         public void catWalk(int location, int direction)
         {
-            //int catLocal = location^2 potentially way to to absolute function
             if (location%frameRate == 0)
             {//this swaps the animation every 8 frames
                 leftLeg = !leftLeg;
             }
-            //leftLeg = false;
             if (leftLeg)
             {
                 // back right leg
@@ -96,38 +102,80 @@ namespace Game10003
             // body
             Draw.FillColor = brightOrange;
             Draw.Capsule(location -30 * direction, 138, location+60 * direction, 136, 13);
+
+            // head
+            Draw.FillColor = darkOrange;
+            Draw.Quad(location + 66 * direction, 124, location + 76 * direction, 116,location + 77 * direction, 132, location + 73 * direction, 140);
+
+            Draw.FillColor = brightOrange;
+            Draw.Triangle(location + 85 * direction, 100, location + 87 * direction, 90, location + 95 * direction, 108);
+            Draw.Circle(location + 86 * direction, 117, 16);
+            Draw.FillColor = Color.OffWhite;
+            Draw.Triangle(location + 90 * direction, 120, location + 95 * direction, 112, location + 100 * direction, 120);
+            Draw.FillColor = Color.Black;
+            Draw.Circle(location + 97 * direction, 118, 2);
         }
 
         public void CatAction()
         {
-            if (catStats[0] < -500 || catStats[0] > 500 || catStats[1] > 500)
-            {
+            if (tempature < -500 || tempature > 500 || hunger > 800)
+            { // here the cat wants something so it paws at the mouse
                 pawTime++;
+                Draw.FillColor = darkOrange;
+                //legs
+                Draw.Quad(catPosition - 5, 150, catPosition - 10, 153, catPosition - 13, 190, catPosition - 8, 190);
+                Draw.Quad(catPosition + 5, 150, catPosition + 10, 153, catPosition + 13, 190, catPosition + 8, 190);
+                
+                //body
+                Draw.FillColor = brightOrange;
+                Draw.Capsule(catPosition, 160, catPosition, 120, 15);
+                //feet
+                Draw.Circle(catPosition - 13, 190, 6);
+                Draw.Circle(catPosition + 13, 190, 6);
+
+                // head
+                Draw.Circle(catPosition, 95, 12);
+
+
+                // paws
                 if (pawTime < frameRate)
                 {
-                    Draw.Circle(50, 50, 10);
+                    Draw.Circle(catPosition - 14, 60, 7);
+                    Draw.Circle(catPosition + 14, 87, 7);
+
+
                 }
                 else if (pawTime < frameRate * 2)
                 {
-                    Draw.Circle(50, 50, 20);
+                    Draw.Circle(catPosition + 14, 60, 7);
+                    Draw.Circle(catPosition - 14, 87, 7);
                 }
                 else pawTime = 0;
             }
             else
-            {
+            { // here the cat is satisfied so it lays down.
                 Draw.FillColor = brightOrange;
-                Draw.Circle(catPosition, 170, 30);
+                Draw.Ellipse(catPosition, 180, 60, 50);
             }
         }
 
         
         public void backgroundDraw()
         {
+            Color Background = Brown;
+            if (tempature < -1100)
+            {
+                Background = coldBrown;
+            }
+            else if (tempature > 1100)
+            {
+               Background = hotBrown;
+            }
             for (int i = 0; i < 30; i++)
             {
                 Draw.FillColor = Color.Black;
                 Draw.Rectangle(i * 20, 8, 2, 180);
-                Draw.FillColor = Brown;
+                Draw.FillColor = Background;
                 Draw.Rectangle(i * 20+2, 8, 18, 180);
             }
 
@@ -140,22 +188,37 @@ namespace Game10003
 
         public void Furniture()
         {
+            
             // heater
             Draw.FillColor = Color.DarkGray;
-            Draw.Rectangle(40, 160, 50, 80);
+            Draw.Rectangle(58, 0, 14, 160);
+            Draw.Rectangle(30, 140, 70, 60);
+            Draw.FillColor = Color.Black;
+            if (heaterWoodLeft > 0)
+            {
+                Draw.FillColor = Color.Red;
+            }
+            
+            Draw.Square(45, 160, 40);
+            
 
             // window
             Draw.FillColor = Color.OffWhite;
             Draw.Rectangle(400, 20, 54, 70);
-            Draw.FillColor = Color.Blue;
-            Draw.Rectangle(402, 55, 50, 30);
             if (windowOpen)
             {
-
+                Draw.FillColor = Color.Blue;
+                Draw.Rectangle(402, 24, 50, 18);
+                Draw.FillColor = windowBlue;
+                Draw.Rectangle(402, 44, 50, 40);
+                Draw.FillColor = Color.LightGray;
+                Draw.Rectangle(402, 55, 50, 18);
             }
             else
             {
-                Draw.FillColor = Color.Blue;
+                Draw.FillColor = windowBlue;
+                Draw.Rectangle(402, 55, 50, 30);
+                Draw.FillColor = windowBlue;
                 Draw.Rectangle(402, 24, 50, 30);
             }
 
@@ -169,26 +232,58 @@ namespace Game10003
             
         }
 
+        public void FoodDraw()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (food[i] != -1)
+                {
+                    Draw.FillColor = Color.LightGray;
+                    Draw.Rectangle(food[i], 192, 20, 8);
+                    Draw.FillColor = brightOrange;
+                    Draw.Circle(food[i] + 10, 196, 3);
+                }
+            }
+        }
+
+        public void DrawUi()
+        {
+            Draw.FillColor = Color.Blue;
+            Draw.Square(550, 0, 50);
+            Draw.FillColor = Color.White;
+            Draw.Square(555, 5, 40);
+            if (held == "wood")
+            {
+                Draw.FillColor = Brown;
+                Draw.Rectangle(560, 20, 20, 10);
+            }
+            else if (held == "food")
+            {
+                Draw.FillColor = Color.LightGray;
+                Draw.Rectangle(564, 20, 20, 8);
+                Draw.FillColor = brightOrange;
+                Draw.Circle(574, 24, 3);
+            }
+        }
         public void Update()
         {
             Window.ClearBackground(Color.White);
             float mouseX = Input.GetMouseX();
+            float mouseY = Input.GetMouseY();
             // draw background
             backgroundDraw();
             //draw furniture
             Furniture();
 
-
             // draw cat
 
             // this makes the cat follow the mouse
-
             if (mouseX > catPosition + 8)// this is +120 so its in the middle of the cat.
             {
                 catPosition += 2;
                 catWalk(catPosition, 1); // this sends the x coordinate to the cat drawing function
             }
-            else if (mouseX < catPosition -8)
+            else if (mouseX < catPosition - 8)
             {
                 catPosition -= 2;
                 catWalk(catPosition, -1); // this sends the x coordinate to the cat drawing function
@@ -198,40 +293,105 @@ namespace Game10003
                 CatAction();
             }
 
-            // update stats
-            //for (int i = 0; i<1;i++)
-            //{
-            //catStats[i] += Random.Integer(-1, 1);
-            //}
-            catStats[0] += Random.Integer(-5, 5);
-            catStats[1] += Random.Integer(-4, 6);
+            //draw food
+            FoodDraw();
+            //draw ui
+            DrawUi();
+
+            // heat and hunger changes.
+            tempature += Random.Integer(-5, 5);
+            hunger += Random.Integer(-4, 6);
             if (windowOpen == true)
-            {
-                catStats[0] -= 1;
+            { // window coolint
+                tempature -= 1;
             }
-            
+
             if (heaterWoodLeft > 0)
-            {
-                catStats[0] += 2;
+            { // heater warming
+                tempature += 2;
+                heaterWoodLeft -= Random.Integer(0, 2);
             }
-            //Input.IsMouseButtonPressed
+
+            // food tracking
+            for (int i = 0; i < 5; i++)
+            {
+                if (catPosition <= food[i] + 10 && catPosition >= food[i] - 10)
+                {
+                    food[i] = -1;
+                    hunger -= 200;
+                }
+            }
+
+            if (hunger < 0)
+            {
+                hunger = 0;
+            }
+
+
             if (Input.IsMouseButtonPressed(MouseInput.Left))
             {
-                Console.WriteLine($"temp {catStats[0]}, food {catStats[1]}");
-                if (mouseX > 400 && mouseX< 450)
+                Console.WriteLine($"temp {tempature}, food {hunger}, Wood {heaterWoodLeft}, food {food[0]}"); // this is for debugging and will be commented out
+                if (held == "none")
                 {
-                    windowOpen = !windowOpen;
-                    Console.WriteLine(windowOpen); // testing
+                    if (mouseX > 400 && mouseX < 450 && mouseY > 20 && mouseY < 90)
+                    {
+                        windowOpen = !windowOpen;
+                        Console.WriteLine(windowOpen); // testing
+                    }
+
+                    if (mouseX > 500 && mouseY > 130)
+                    {
+                        if (mouseX > 550)
+                        {
+                            held = "food";
+                        }
+                        else
+                        {
+                            held = "wood";
+                        }
+
+                    }
                 }
-
-
-                if (mouseX > 500)
+                else if (held == "wood")
                 {
-                    catStats[1] = 0;
+                    if (mouseX > 500 && mouseX < 550 && mouseY > 130)
+                    {
+                        held = "none";
+                    }
+                    else if (mouseX < 100 && mouseX > 30 && mouseY > 140)
+                    {
+                        held = "none";
+                        heaterWoodLeft += 150;
+                    }
+                }
+                else if (held == "food")
+                {
+                    if (mouseX > 550 && mouseY > 130)
+                    {
+                        held = "none";
+                    }
+                    else
+                    {
+                        bool quitLoop = false;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            if (catPosition <= food[i] - 5 && catPosition >= food[i] + 5)
+                            {
+                                food[i] = -1;
+                                hunger -= 300;
+                            }
+                            if (food[i] == -1 && quitLoop == false)
+                            {
+                                food[i] = (int)mouseX;
+                                quitLoop = true;
+                                held = "none";
+                            }
+                        }
+                    }
                 }
             }
             // add feeding mechanic
-            
+
         }
     }
 }
